1192f76862a8ed81258bf067ccd3c66ef16e59dd
FavourtheDev <favourthedeveloper@gmail.com>
2023-07-21 02:17:37 +0100

    Added more JS Codes

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const result = document.getElementById("result");
 button.addEventListener('click', getInfo);
 
 function getInfo() {
+    if (dateInput.value === "") {
+        alert("Input your Date of Birth");
+    } else {
     const userDateInput = new Date(dateInput.value);
     const userYear = userDateInput.getFullYear();
     const userMonth = userDateInput.getMonth() + 1;
@@ -15,5 +18,11 @@ function getInfo() {
     const presentMonth = (presentDate.getMonth() + 1) - userMonth;
     const presentDay = presentDate.getDate() - userDay;
 
+    document.body.style.backgroundColor = "Green";
+    setInterval(() => document.body.style.backgroundColor = "blue", 3000)
+
     result.innerHTML = `You are ${presentYear} years and ${presentMonth} months old`;
+    dateInput.value = "";
+}
+
 }
